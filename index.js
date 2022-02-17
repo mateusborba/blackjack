df10b49973aabd76ce19ec3951577c7ccdeb6187
Mateus Borba <mateus.santos@madeinweb.com.br>
2022-02-17 14:20:51 -0300

    fifth-commit

--- index.js
+++ index.js
@@ -11,9 +11,53 @@ function createDraw(baralho) {
     return baralho[cartaAleatoria];
 }
 
+
+
 //Faz a soma das cartas que o usuário possui 
 function getHandValue(hand) {
+    /*
+    hand = [
+        2 (posicao 0),
+        5 (posicao 1),
+        7 (posicao 2)
+    ]
+    */
     var sum = 0;
+
+    /*
+    primera vez que chega aqui:
+    sum = 0
+    i = 0
+    #INICIO DO LAÇO / LOOP
+    verificar a condicao => i (0) < hand.length (3) = true
+    se true, ele faz o que ta dentro
+    sum = sum + hand[i] (hand na posicao i (0), logo, hand[i] = 2)
+    logo, sum = 0 + 2 = 2
+    por fim, ele faz o i++ = i + 1, logo i = 1
+    #FIM DO LOOP
+
+    segunda vez do loop:
+    i < hand.length => 1 < 3 = true
+    se true, faz ...
+    sum = sum + hand[i]
+    logo, sum = 2 + 5 (hand[i], i = 1, logo hand[1] = 5)
+    sum = 7
+    por fim, ele faz o i++, agora o i = 2
+
+    terceira vez:
+    i < hand.length => 2 < 3 = true
+    se true ...
+    sum = sum + hand[i]
+    sum = 7 + 7 (hand[i], i = 2, logo, hand[2] = 7)
+    sum = 14
+    por fim, ele faz o i++, agora o i = 3
+
+    quarta vez:
+    i < hand.length => 3 < 3 = false
+    logo, ele sai do for
+    */
+
+
     for (var i = 0; i < hand.length; i++) {
         sum += hand[i];
     }
@@ -104,6 +148,16 @@ function stand() {
             return resetGame();
         }, 800);
     }
+    else if (getHandValue(table == getHandValue(playerHand))) {
+        setTimeout(() => {
+            Swal.fire({
+                icon: 'alert',
+                title: 'The table tied with you',
+                text: 'You scored ' + getHandValue(playerHand) + ' and ' + 'Table scored ' + getHandValue(tableHand),
+            })
+            return resetGame();
+        }, 800);
+    }
     document.getElementById("table_hand").innerHTML = tableHand;
     document.getElementById("table_hand_value").innerHTML = getHandValue(tableHand);
 
@@ -121,4 +175,12 @@ function resetGame() {
     document.getElementById("player_hand_value").innerHTML = playerHand;
     document.getElementById("table_hand").innerHTML = tableHand;
     document.getElementById("table_hand_value").innerHTML = tableHand;
+}
+
+function disableButton() {
+    document.getElementById("btn-start").disabled = false;
+    document.getElementById("btn-hit").disabled = true;
+    document.getElementById("btn-stand").disabled = true;
+    document.getElementById("btn-reset").disabled = true;
+    return disableButton();
 }
